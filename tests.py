a0625b25c795c0982a7dda65dbaadeed3d2a7aa8
felipeinteli <felipe.moura@sou.inteli.edu.br>
2023-11-10 19:39:05 -0300

    teste

--- tests.py
+++ tests.py
@@ -9,54 +9,54 @@ from appp import Appp
 class TestApppp(unittest.TestCase):
 
     def test_create_animal(self):
-        Appp = Appp()
+        app_instance = Appp()
 
-        animal = Appp.create_animal()
+        animal = app_instance.create_animal()
 
         self.assertIsNotNone(animal)
         self.assertEqual(animal.name, "Simba")
         self.assertEqual(animal.species, "Leão")
 
     def test_feed_animal(self):
-        Appp = Appp()
-        hApppiness_level = Appp.feed_animal()
+        app_instance = Appp()
+        hApppiness_level = app_instance.feed_animal()
         self.assertEqual(hApppiness_level, 50)  
 
     def test_criar_recinto(self):
-        Appp = Appp()
-        enclosure = Appp.criar_recinto()
+        app_instance = Appp()
+        enclosure = app_instance.criar_recinto()
         self.assertIsNotNone(enclosure)
         self.assertEqual(enclosure.name, "Savana Africana")
         self.assertEqual(enclosure.species, "Leão")
 
     def test_add_animal_to_enclosure(self):
-        Appp = Appp()
-        enclosure = Appp.add_animal_to_enclosure()
+        app_instance = Appp()
+        enclosure = app_instance.add_animal_to_enclosure()
         self.assertIsNotNone(enclosure)
         self.assertTrue(enclosure.has_animal("Simba"))
 
     def test_cuidar_bem_recinto(self):
-        Appp = Appp()
-        enclosure = Appp.cuidar_bem_recinto()
+        app_instance = Appp()
+        enclosure = app_instance.cuidar_bem_recinto()
         self.assertIsNotNone(enclosure)
         self.assertTrue(enclosure.is_well_maintained)
 
     def test_cuidar_mal_recinto(self):
-        Appp = Appp()
-        enclosure = Appp.cuidar_mal_recinto()
+        app_instance = Appp()
+        enclosure = app_instance.cuidar_mal_recinto()
         self.assertIsNotNone(enclosure)
         self.assertFalse(enclosure.is_well_maintained)
 
     def test_get_an_animal_felicidade(self):
-        Appp = Appp()
-        hApppiness = Appp.get_an_animal_felicidade()
+        app_instance = Appp()
+        hApppiness = app_instance.get_an_animal_felicidade()
         self.assertIsNotNone(hApppiness)
         # Add assertions based on expected hApppiness level
 
     def test_ganhar_dinheiro(self):
-        Appp = Appp()
+        app_instance = Appp()
         # Test the ganhar_dinheiro function
-        money_earned = Appp.ganhar_dinheiro()
+        money_earned = app_instance.ganhar_dinheiro()
         self.assertIsNotNone(money_earned)
         # Add assertions based on expected money earned
 
